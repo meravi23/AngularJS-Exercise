7978f440d66d20f9568cdd721d4df23f490158ec
Merav <merav.mi@gmail.com>
2019-06-10 13:17:18 +0300

    added actor constructor

--- actorsCtrl.js
+++ actorsCtrl.js
@@ -1,5 +1,11 @@
 app.controller("actorsCtrl", function($scope) {
     //$scope.test = "blabla";
-
+    function Actor(fname, lname, image, birthday, imdbLink) {
+        this.fname = fname;
+        this.lname = lname;
+        this.image = image;
+        this.birthday = birthday;
+        this.imdbLink = imdbLink;
+    }
 
 });
