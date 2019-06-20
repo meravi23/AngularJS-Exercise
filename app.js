61cc8fe0a3da7154007d07c92e166a7073a3b835
Merav <merav.mi@gmail.com>
2019-06-20 15:56:04 +0300

    active navbar link background partially ok

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ app.config(function ($routeProvider) {
         when("/", {
             templateUrl: "home.html",
             controller: "homeCtrl"
+            
         }).when("/actors", {
             templateUrl: "actors.html",
             controller: "actorsCtrl"
+            
         }).when("/movies", {
             templateUrl: "movies.html",
             controller: "moviesCtrl"
+            
         }).when("/movies/:id", {
             templateUrl: "movieDetails.html",
             controller: "movieDetailsCtrl"
+            
         });
 });
--- navCtrl.js
+++ navCtrl.js
@@ -1,4 +1,14 @@
 app.controller("navCtrl", function ($scope) {
 
-    
+    $scope.links = ["Actors", "Movies"];
+
+    $scope.selectedLink = null;
+    $scope.onSelectLink = function (link) {
+        if ($scope.selectedLink === link) {
+            $scope.selectedLink = null;
+        } else {
+            $scope.selectedLink = link;
+        }
+    }
+
 });
